1d78b765c3e065caa7f5a1deec76f3275c7c0ce9
Suhyun Lim <sh9742@MacBook-Air-7.local>
2024-11-02 22:54:02 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Chandrika Deb',
+    title: 'Suhyun Lim',
     description:
-      'Chandrika Deb is an IIM alumnus who spearheads marketing initiatives at Tata Steel and is also a freelance tech blogger.',
-    siteUrl: 'https://chandrikadeb7.github.io', // No trailing slash allowed!
+      'Suhyun Lim is chaing her career track from business consulting to technology development.',
+    siteUrl: 'https://ellen-creator.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@chandrikadeb7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Chandrika Deb',
-        short_name: 'Chandrika Deb',
+        name: 'Suhyun Lim',
+        short_name: 'Suhyun Lim',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
