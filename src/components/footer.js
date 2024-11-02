23cf1e5d3d645020b08f10d411dead7ad3752fc4
Suhyun Lim <sh9742@MacBook-Air-7.local>
2024-11-02 23:59:37 +0900

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,10 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang<br></br>
-          Revised by Chandrika Deb
-          </div>
+        <a href="">
+
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
